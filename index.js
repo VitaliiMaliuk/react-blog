2e509a787f811ecfc357908aba38e0a4a27790ea
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-03-25 20:35:51 +0200

    init: start blog task

---
+++ index.js
@@ -0,0 +1,135 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://maliukvitalii:7557558181@cluster0.v1eecw8.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB ok");
+  })
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Invalid username or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Unable to authorize",
+    });
+  }
+});
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Registration failed",
+    });
+  }
+});
+
+app.get("/auth/me", checkAuth, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const { passwordHash, ...userData } = user._doc;
+    res.json(userData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Authorization failed",
+    });
+  }
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server OK");
+});
