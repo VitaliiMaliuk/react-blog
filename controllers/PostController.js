6ab22a0d5508ca85161dd5b9f02c1a30af02d2d1
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-03-29 18:57:31 +0300

    feat: fix create function

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -25,7 +25,7 @@ export const getOne = async (req, res) => {
       {
         new: true,
       }
-    ).exec();
+    ).populate("user").exec();
     if (!doc) {
       return res.status(404).json({
         message: "Article not found",
@@ -70,7 +70,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(","),
       user: req.userId,
     });
     const post = await doc.save();
